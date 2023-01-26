0f511c369316b0692c4bc4277d9992d2f547f7aa
Paulo Ribeiro <plribeiro3000@gmail.com>
2023-01-26 10:22:46 -0300

    feat(*): Initial Commit

---
+++ lib/validates_rfc.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "validates_rfc/version"
+
+module ValidatesRfc
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/validates_rfc_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ValidatesRfc do
+  it "has a version number" do
+    expect(ValidatesRfc::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
